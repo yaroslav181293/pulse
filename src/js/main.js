297699fe8015ca6658ab3b7564fd7f3266168ec6
Yaroslav <tigr181993@gmail.com>
2021-12-15 22:38:25 +0200

    добавлено слайдер блок 4

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,28 @@
-const sum = require("./module/sum.js");
-console.log(sum(2,10));
-console.log(sum(30,10));
+$(document).ready(function () {
+    $('.carousel__inner').slick({
+           
+        prevArrow:'<button type="button" class="slick-prev"><img src="img/left.svg"></button>',
+        nextArrow:'<button type="button" class="slick-next"><img src="img/right.svg"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll:1 ,
+                arrows:false,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll:1,
+                dots:true,
+                arrows:false
+              }
+            }
+           
+        ]
+    });
+  });
