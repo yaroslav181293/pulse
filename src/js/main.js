988e732b39d29411d77e5605cc107d1bea42afab
Yaroslav <tigr181993@gmail.com>
2021-12-23 01:15:02 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -44,4 +44,43 @@ $(document).ready(function () {
         $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
       })
     })
+
+    // Modal
+    $('[data-modal=consultation]').on('click', function(){
+      $('.overlay,#consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function(){
+      $('.overlay,#consultation,#thanks,#order').fadeOut('slow');
+    });
+   
+    $('.button_mini').each(function(i){
+      $(this).on('click', function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay,#order').fadeIn('slow');
+      })
+    });
+   
+    function validateForms(form){
+      $(form).validate({
+        rules:{
+          name:"required",
+          phone:"required",
+          email:{
+            required:true,
+            email:true
+          }
+        },
+          messages: {
+            name: "Пожалуйста,введите свое имя",
+            phone:"Пожалуйста,введите свой номер  телефона",
+            email: {
+              required: "Пожалуйста,введите свою почту ",
+              email: "Неправильно введен адрес почты "
+            }
+        }
+      });
+    };
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
   });
