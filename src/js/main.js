85980e5128d4a99ebed7642816b88b3d37a09725
Yaroslav <tigr181993@gmail.com>
2021-12-23 20:48:02 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -83,4 +83,37 @@ $(document).ready(function () {
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    $('form').submit(function(e){
+      e.preventDefault();
+      $.ajax({
+        type: "POST",
+        URL:"mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function(){
+        $(this).find("input").val("");
+
+
+        $('form').trigger('reset');
+      });
+      return false;
+    });
+
+
+    // scroll
+    $(window).scroll(function(){
+      if($(this).scrollTop() > 1600){
+        $('.pageup').fadeIn();
+      } else{
+        $('.pageup').fadeOut();
+      }
+
+    });
+
+    $("a[href^='#']").click(function(){
+      const _href = $(this).attr("href");
+      $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+      return false;
+  });
+
   });
